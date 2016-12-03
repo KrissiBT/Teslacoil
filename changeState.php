7b65e380d5c03a44486d5d3c998b47b51d056ea2
KrissiBT <minecraft.man96@gmail.com>
2016-12-03 17:59:49 +0000

    Revert "Revert "added a phph""
    This reverts commit 2b03644095d5db799f617a2c989a189c14e5c188.

---
+++ changeState.php
@@ -0,0 +1,17 @@
+<?php
+$file = 'stat.txt';
+// Open the file to get existing content
+$current = file_get_contents($file);
+// Append a new person to the file
+if ($current == "1") {
+  $current = "0";
+  // Write the contents back to the file
+  file_put_contents($file, $current);
+}
+else {
+  $current = "1";
+  // Write the contents back to the file
+  file_put_contents($file, $current);
+
+}
+?>
